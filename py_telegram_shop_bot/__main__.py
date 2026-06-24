57f3e2b947319077a6d0f252fc1812181794bfc4
OlegShiro <olegshiro@mail.ru>
2026-06-24 08:54:13 +0700

    Initial commit

---
+++ py_telegram_shop_bot/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Python Telegram Shop Bot
+ID: Project_00106261d
+
+Простой телеграм бот на Python 3 с реализацией функционала интернет-магазина
+цифровых товаров и возможностью оплаты через различные платёжные сервисы.
+"""
+
+print("Python Telegram Shop Bot by Oleg Shiro.")
